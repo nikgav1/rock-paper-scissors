40f980e2af0817c570c96364cc8445d04c1ba52e
nikgav1 <nikgav66@gmail.com>
2025-03-27 22:37:43 +0300

    Added Ui handling

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let humanScore = 0;
 const numberOfWins = 3;
 
 const container = document.getElementById("container")
+const score = document.getElementById("score")
 
 function humanChoice(){
     container.addEventListener("click", (e) =>{
@@ -32,15 +33,15 @@ function computerChoice(){
 
 function roundHandler(humanChoice, computerChoice){
     if (humanChoice === computerChoice){
-        
+        score.textContent = `${humanScore} : ${computerScore}`
     } 
     else if (humanChoice === "Rock" && computerChoice === "Scissors" || humanChoice === "Paper" && computerChoice === "Rock" || humanChoice === "Scissors" && computerChoice === "Paper"){
         humanScore += 1;
-        
+        score.textContent = `${humanScore} : ${computerScore}`
     }
     else if (computerChoice === "Rock" && humanChoice === "Scissors" || computerChoice === "Paper" && humanChoice === "Rock" || computerChoice === "Scissors" && humanChoice === "Paper"){
         computerScore += 1;
-        
+        score.textContent = `${humanScore} : ${computerScore}`
     }
 }
 
